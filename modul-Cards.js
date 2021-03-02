ee5c868356b6cb7e99e7ede58cfe3ac9ea41f954
RomanSamynin <74867130+RomanSamynin@users.noreply.github.com>
2021-03-02 19:25:26 +0200

    Update modul-Cards.js

--- modul-Cards.js
+++ modul-Cards.js
@@ -1,12 +1,14 @@
 //  class for cards
-
+import {getResource} from '../services/services';
+function cards() {
     class MenuCard {
-        constructor(src, alt, title, descr, price, parentSelector) {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
+            this.classes = classes;
             this.parent = document.querySelector(parentSelector);
             this.transfer = 28;
             this.changeToUAH();
@@ -18,6 +20,14 @@
 
         render() {
             const element = document.createElement('div');
+            
+             if (this.classes.length === 0) {
+                this.classes = "menu__item";
+                element.classList.add(this.classes);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+            
             element.innerHTML = `
             <div class="menu__item">
                 <img src=${this.src} alt=${this.alt}>
@@ -35,11 +45,12 @@
         }
     }
 
-    new MenuCard(
-        "img/tabs/vegy.jpg",
-        "vegy",
-        'Меню "Фитнес"',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-        9,
-        '.menu .container'
-    ).render();
+    getResource('http://localhost:3000/menu')
+        .then(data => {
+            data.forEach(({img, altimg, title, descr, price}) => {
+                new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
+            });
+        });
+}
+
+export default cards;
